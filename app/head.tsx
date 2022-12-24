e4c3a5fa12934d875af353f1e128566a318a4304
Lazar Nikolov <hello@nikolovlazar.com>
2022-12-24 21:33:14 +0100

    added viewport meta to fix responsiveness

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Flashcards! 🧠</title>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     </>
   );
 }
