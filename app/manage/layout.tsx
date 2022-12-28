6fc1a73bba60e75aca67c95965bab19a8b02dc81
Lazar Nikolov <hello@nikolovlazar.com>
2022-12-28 18:30:51 +0100

    fixed mobile layout issue

--- app/manage/layout.tsx
+++ app/manage/layout.tsx
@@ -8,12 +8,12 @@ import { ManageSidebar } from '../../src/components/sidebars';
 export default function Layout({ children }: { children: React.ReactNode }) {
   const isSmall = useBreakpointValue({ base: true, md: false });
   const pathname = usePathname();
-  const showSidebar = pathname === '/manage' || !isSmall;
+  const showSidebar = pathname?.startsWith('/manage') || !isSmall;
 
   return (
     <HStack height='100vh' alignItems='flex-start' flex={1} spacing={0}>
       {showSidebar && <ManageSidebar />}
-      {(!pathname?.endsWith('/manage') || !isSmall) && (
+      {(!pathname?.startsWith('/manage') || !isSmall) && (
         <Box bg='pageDottedBackground' bgSize='16px 16px' w='full' h='full'>
           {children}
         </Box>
