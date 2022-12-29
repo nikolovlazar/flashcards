c95d372bbfeb9f96c4f9959073792a805f4e6945
Lazar Nikolov <hello@nikolovlazar.com>
2022-12-29 16:10:22 +0100

    fixed middleware auth check

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import { withAuth } from 'next-auth/middleware';
 export default withAuth({
   callbacks: {
     authorized: async ({ req }) => {
-      const hasSession = !!req.cookies._parsed.get('next-auth.session-token');
+      const hasSession =
+        !!req.cookies._parsed.get('next-auth.session-token') ||
+        !!req.cookies._parsed.get('__Secure-next-auth.session-token');
       return hasSession;
     },
   },
