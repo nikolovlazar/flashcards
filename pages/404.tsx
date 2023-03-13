ae5f546f75c47c87a1c4f6473a523367bc3558d4
Lazar Nikolov <lazar.nikolov@sentry.io>
2023-03-13 15:41:12 -0400

    added sentry capture when 404 happens

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,17 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { captureMessage } from '@sentry/nextjs';
+
 export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!!router.asPath) {
+      captureMessage(`404 - ${router.asPath}`, {
+        level: 'error',
+      });
+    }
+  }, [router]);
+
   return <h1>404 - Page Not Found</h1>;
 }
