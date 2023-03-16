6eb48e6987ab8edc90f4ddd0bf8febd72151ff13
Lazar Nikolov <lazar.nikolov@sentry.io>
2023-03-16 15:23:40 -0400

    replaced localhost with env variable

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,19 +21,16 @@ export const authOptions = {
         password: { label: 'Password', type: 'password' },
       },
       authorize: async (credentials, req) => {
-        const user = await fetch(
-          `http://localhost:3000/api/user/check-credentials`,
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              Accept: 'application/json',
-            },
-            body: Object.entries(credentials)
-              .map((e) => e.join('='))
-              .join('&'),
-          }
-        )
+        const user = await fetch(`${NEXTAUTH_URL}/api/user/check-credentials`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Accept: 'application/json',
+          },
+          body: Object.entries(credentials)
+            .map((e) => e.join('='))
+            .join('&'),
+        })
           .then((res) => res.json())
           .catch((err) => {
             return null;
