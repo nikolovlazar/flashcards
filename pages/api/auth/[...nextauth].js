4ef8bd2248d4793d16f93dbffafdc7fafe0269a2
Lazar Nikolov <lazar.nikolov@sentry.io>
2023-03-16 15:16:08 -0400

    removed github auth provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 import checkCredentials from '../user/check-credentials';
@@ -10,10 +9,6 @@ import prisma from '../../../prisma';
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     CredentialsProvider({
       id: 'credentials',
       name: 'credentials',
