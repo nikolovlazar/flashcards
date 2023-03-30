0acf65f3b4d367e2aa7bf1802210e7584b0af453
Lazar Nikolov <hello@nikolovlazar.com>
2023-03-30 12:38:45 -0400

    implementing Sentry and Distributed Tracing

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+const { withSentryConfig } = require('@sentry/nextjs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -18,3 +24,5 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = withSentryConfig(module.exports, { silent: true });
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,7 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-
-import checkCredentials from '../user/check-credentials';
+import { checkCredentials } from '../../../utils/check-credentials';
 
 import prisma from '../../../prisma';
 
@@ -21,27 +20,13 @@ export const authOptions = {
         password: { label: 'Password', type: 'password' },
       },
       authorize: async (credentials, req) => {
-        const user = await fetch(
-          `${req.headers.origin}/api/user/check-credentials`,
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              Accept: 'application/json',
-            },
-            body: Object.entries(credentials)
-              .map((e) => e.join('='))
-              .join('&'),
-          }
-        )
-          .then((res) => res.json())
-          .catch((err) => {
-            return null;
-          });
-
-        if (user) {
+        try {
+          const user = await checkCredentials(
+            credentials.email,
+            credentials.password
+          );
           return user;
-        } else {
+        } catch (e) {
           return null;
         }
       },
