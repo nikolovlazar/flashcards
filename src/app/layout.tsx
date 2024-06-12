0cae6764e64c416469739231834374bf9e693fc7
Joe <joemmalatesta@gmail.com>
2024-06-12 13:58:21 -0700

    z-40... fix nav bar layering issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         )}
       >
         <div className='flex min-h-screen w-full flex-col'>
-          <header className='sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
+          <header className='sticky z-40 top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
             <nav className='hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
               <Link
                 href='/'
