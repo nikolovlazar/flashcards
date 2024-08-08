5a858dc7a7d7e0da5c1049da8342e9d48248babf
Lazar Nikolov <lazar.nikolov@sentry.io>
2024-08-08 17:39:28 -0400

    woah (look at commit description)
    - new project structure
    - docker compose
    - postgresql database
    - initial Go REST API project

--- packages/web/prisma/seed/data.ts
+++ packages/web/prisma/seed/data.ts
@@ -3,11 +3,10 @@ import slugify from "slugify";
 
 export const categories = Array(40)
   .fill(0)
-  .map((_, i) => {
+  .map(() => {
     const name = faker.company.name();
     const slug = slugify(name, { lower: true });
     return {
-      id: i,
       name,
       slug,
     };
@@ -15,14 +14,13 @@ export const categories = Array(40)
 
 export const flashcards = Array(250)
   .fill(0)
-  .map((_, i) => {
+  .map(() => {
     const question = faker.commerce.productName();
     const slug = slugify(question, { lower: true });
-    const categoryId = faker.number.int({ min: 0, max: categories.length - 1 });
+    const categoryId = faker.number.int({ min: 1, max: categories.length - 1 });
     const answer = faker.commerce.department();
 
     return {
-      id: i,
       question,
       answer,
       categoryId,
--- packages/web/prisma/seed/index.ts
+++ packages/web/prisma/seed/index.ts
@@ -1,8 +1,13 @@
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 import { flashcards, categories } from "./data";
 
-const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   await prisma.category.createMany({ data: categories });
