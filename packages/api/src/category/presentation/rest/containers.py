7bf72d0c556e032a96398b77799c45a3c47fb05b
Lazar Nikolov <lazar.nikolov@sentry.io>
2024-11-14 11:24:22 -0500

    fixing more bugs

--- packages/api/src/category/presentation/rest/containers.py
+++ packages/api/src/category/presentation/rest/containers.py
@@ -1,8 +1,9 @@
 from category.application.use_case.command import CategoryCommand
 from category.application.use_case.query import CategoryQuery
 from category.infra.database.repository.rdb import CategoryRepository
+from flashcard.presentation.rest.containers import flashcard_repo
 
 category_repo: CategoryRepository = CategoryRepository()
 
-category_query: CategoryQuery = CategoryQuery(category_repository=category_repo)
+category_query: CategoryQuery = CategoryQuery(category_repository=category_repo, flashcard_repository=flashcard_repo)
 category_command: CategoryCommand = CategoryCommand(category_repository=category_repo)
