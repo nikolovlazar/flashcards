53b8530af2f9ed525bb7dc6b3eaed2caa24b9374
Lazar Nikolov <lazar.nikolov@sentry.io>
2024-11-20 11:58:45 -0500

    Setting DEBUG to False

--- packages/api/src/shared/infra/django/settings.py
+++ packages/api/src/shared/infra/django/settings.py
@@ -20,6 +20,7 @@ env.read_env(".env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
+DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
